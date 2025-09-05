7c1a54031f6e2f154f8ba372a65c1bc61396470a
Felix Uhl <github@mail.felix-uhl.de>
2025-09-05 12:28:32 +0200

    Build uiua from source again, upgrade to version 0.17.0-rc.1
    There was a bug in anti-keep with a fill value, so I had to upgrade for day 8 to work out.

--- uiua/flake.nix
+++ uiua/flake.nix
@@ -2,21 +2,28 @@
   description = "A very basic flake";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/e6f23dc08d3624daab7094b701aa3954923c6bbb"; # Uiua 0.16.2
   inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.uiua = {
+    url = "github:uiua-lang/uiua";
+    inputs.nixpkgs.follows = "nixpkgs";
+    inputs.flake-utils.follows = "flake-utils";
+  };
 
   outputs =
     {
       self,
       nixpkgs,
       flake-utils,
+      uiua,
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        uiua-pkg = uiua.packages.${system}.default;
       in
       {
         devShell = pkgs.mkShellNoCC {
-          packages = [ pkgs.uiua ];
+          packages = [ uiua-pkg ];
         };
       }
     );
